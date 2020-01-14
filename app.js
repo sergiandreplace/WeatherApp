6e3ce319da052029865a6da66841ad1a4cd154a6
Sergi Martínez <sergi.martinez@scmspain.com>
2020-01-14 19:32:08 +0100

    we get coordinates

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+}
+
+function showPosition(position) {
+    console.log(position)
+}
+
+function showError(error) {
+    console.log(error)
+}
+getLocation();
